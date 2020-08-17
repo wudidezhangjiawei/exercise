cf518478a7347810dc329659a268617edead4ab9
zhangjiawei <544702452@qq.com>
2020-08-17 18:02:22 +0800

    配置和生成迁移完成

--- bookmanage/bookmanage/settings.py
+++ bookmanage/bookmanage/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'bookmanage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1', # 数据库主机
+        'PORT': 3306, # 数据库端口
+        'USER': 'root', # 数据库用户名
+        'PASSWORD': 'mysql', # 数据库用户密码
+        'NAME': 'books' # 数据库名字
     }
 }
 
