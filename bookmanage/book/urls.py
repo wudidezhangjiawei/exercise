fd453b1291ea38557f8f06d4a20cc40716b67802
zhangjiawei <544702452@qq.com>
2020-08-17 22:05:44 +0800

    httpresponse练习

--- bookmanage/book/urls.py
+++ bookmanage/book/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from  book.views import index,goods
+from  book.views import index,goods,get,post,post_json
 urlpatterns = [
     path('index/',index),
-    path('<cat__id>/<goods__id>/',goods)
+    path('<cat__id>/<goods__id>/',goods),
+    path('get/',get),
+    path('post/',post),
+    path('post_json/',post_json)
 ]
--- bookmanage/book/views.py
+++ bookmanage/book/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
 from  django.http import HttpResponse,JsonResponse
+import json
 
 # Create your views here.
 def index(request):
     return HttpResponse('ok')
 def goods(request,cat__id,goods__id):
     return JsonResponse({'cat__id':cat__id,'goods__id':goods__id})
+def get(request):
+    a = request.GET.get('a')
+    b = request.GET.get('b')
+    alist = request.GET.getlist('a')
+    print(a)
+    print(b)
+    print(alist)
+    return  HttpResponse("ojbk")
+def post(request):
+    a = request.POST.get('a')
+    b = request.POST.get('b')
+    alist = request.POST.getlist('a')
+    print(a)
+    print(b)
+    print(alist)
+    return  HttpResponse("ojbk")
+def post_json(request):
+    json_str = request.body
+    json_str = json_str.decode()
+    req_data = json.loads(json_str)
+    print(req_data['a'])
+    print(req_data['b'])
+    return  HttpResponse('ok')
